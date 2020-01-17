09e6049a544463b20f51b22800a7365b028380a0
Nicole Washington <mnicolew@uw.edu>
2020-01-16 21:14:59 -0800

    initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <main>
+    <header>
+      <img className="logo" 
+        src="https://static.thenounproject.com/png/1797692-200.png" alt="loose lips logo"
+      />
+      Loose Lips
+    </header>
+    <TextInput/>
+  </main>
+}
+
+function TextInput(){
+  const [text, setText] = useState('')
+
+  return <div className='text-input'>
+    <form 
+      onSubmit={(e)=> {
+        e.preventDefault()
+        setText('')
+      }}
+    className='form'>
+
+      <input value={text} 
+        placeholder=" type message here"
+        onChange={e=> setText(e.target.value)} 
+      />
+
+      <button className='send-button'>
+        <img className= "send-icon" src="https://static.thenounproject.com/png/383448-200.png" alt="send icon"/>
+      </button>
+    </form>
+  </div>
 }
 
 export default App;
