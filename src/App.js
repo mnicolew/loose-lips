a649af6b5c73a4afef28a2a75a5fd8f0c94ffd71
Nicole Washington <mnicolew@uw.edu>
2020-01-22 10:00:55 -0800

    username components

--- src/App.js
+++ src/App.js
@@ -2,27 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import NamePicker from './namePicker';
+
 
 function App() {
-
+  const [messages, setMessages] = useState([])
+  const[name, setName] = useState('')
+  console.log(messages)
   return <main>
     <header>
+      <div style={{display:'flex',alignItems:'center'}}>
       <img className="logo" 
         src="https://static.thenounproject.com/png/1797692-200.png" alt="loose lips logo"
       />
       Loose Lips
+      </div>
+      <NamePicker onSave={setName} />
     </header>
-    <TextInput/>
+
+    <div className="field">
+      {messages.map((m, i)=>{
+        return <div key={i} className='message-wrap'>
+        <div className='message'>{m}</div>
+        </div>
+      })
+    }
+    </div>
+    <TextInput onSend={(m)=>{
+      setMessages([m, ...messages])
+    }}/>
   </main>
 }
 
-function TextInput(){
+function TextInput(props){
   const [text, setText] = useState('')
 
   return <div className='text-input'>
     <form 
       onSubmit={(e)=> {
         e.preventDefault()
+        if (text) props.onSend(text)
         setText('')
       }}
     className='form'>
---
+++ src/namePicker.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import { useState,useRef } from 'react';
+import { FiEdit, FiSave } from 'react-icons/fi';
+// import './App.css'
+
+
+function NamePicker(props) {
+    const[name,setName]= useState('')
+    const[showName, setShowName] = useState(false)
+    const inputEl= useRef(null)
+
+
+    function save() {
+        // inputEl.current.focus()
+        if (name && !showName) {
+            props.onSave(name)
+            localStorage.setItem('name', name)
+        }
+        setShowName(!showName)
+    }
+
+    useEffect(()=>{
+        const n = localStorage.getItem('name')
+        if(n)
+            setName(n)
+            save()
+    }, [])
+
+    return <div>
+        {!showName && <input value={name} ref={inputEl}
+            className='name-input'
+            style={{display: showName ? 'hidden' : 'visible'}}
+            onChange={e=> setName(e.target.value)}
+            onKeyPress={e=> {
+                if (e.key==='Enter') props.onSave(name)
+            }}
+        />}
+        {showName && <div>{name}</div>}
+        <button onClick={save
+            // inputEl.current.focus()
+            // if (name)
+            // props.onSave(name)
+            // setShowName(!showName)
+        }>
+            { showName ? <FiEdit/> : <FiSave/> }
+        </button>
+    </div>
+}
+
+export default NamePicker
